101ebd2b07bddb6048241f959c079a3ccd3c867d
Geoff-Robin <jeffrobin132004@gmail.com>
2025-11-24 23:24:52 +0530

    mounting mcp on fastapi web app

--- recommendation_service/main.py
+++ recommendation_service/main.py
@@ -1,88 +1,19 @@
-from mcp.server.fastmcp import FastMCP
+from fastapi import FastAPI
+import contextlib
+from rec_sys_mcp import mcp
 import os
-from groq import AsyncGroq
-from dotenv import load_dotenv
-from models import WeeklyMealPlan
-from recommendation_controller import get_recommendation
-import prompts
-import json
-import logging
-import sys
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler(sys.stdout), logging.FileHandler("mcp_server.log")],
-)
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with contextlib.AsyncExitStack() as stack:
+        await stack.enter_async_context(mcp.session_manager.run())
+        yield
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+app = FastAPI(lifespan=lifespan)
+app.mount("/", mcp.streamable_http_app(), name="mcp")
 
-
-load_dotenv()
-
-logger.info("Initializing MCP Server for RecSys")
-mcp = FastMCP("MCP Server for RecSys")
-
-
-@mcp.tool()
-async def get_recommendation_tool(match: str):
-    logger.info(f"Tool 'get_recommendation' invoked with match: '{match}'")
-    try:
-        results = await get_recommendation(match)
-        logger.info(f"Returning {len(results)} recommendations")
-        return results
-    except Exception as e:
-        logger.error(f"Error in get_recommendation tool: {e}", exc_info=True)
-        raise
-
-
-@mcp.tool()
-async def get_meal_plan_tool(likes: str, dislikes: str):
-    logger.info(f"Tool 'get_meal_plan' invoked with parameters: match- '{likes}', mismatch- '{dislikes}'")
-    try:
-        search_results = await get_recommendation(likes, dislikes)
-        filtered_results = []
-        for result in search_results:
-            filtered_results.append({
-                "title": result["title"],
-                "directions": result["directions"],
-                "ingredients": result["NER"]
-            })
-        client = AsyncGroq(api_key=os.getenv("GROQ_API_KEY"))
-        response = await client.chat.completions.create(
-            model="openai/gpt-oss-20b",
-            messages=[
-                {
-                    "role": "system",
-                    "content": prompts.SYSTEM_PROMPT,
-                },
-                {
-                    "role": "user",
-                    "content": f"Recipes DataFrame:\n{str(filtered_results)}\n\n\nDisclude Recipes with Ingredients: {dislikes}",
-                },
-            ],
-            response_format={
-                "type": "json_schema",
-                "json_schema": {
-                    "name": "weekly_meal_plan",
-                    "schema": WeeklyMealPlan.model_json_schema(),
-                },
-            },
-        )
-        raw_content = response.choices[0].message.content
-        meal_plan_json = json.loads(raw_content)
-        return meal_plan_json
-    except Exception as e:
-        logger.error(f"Error in get_meal_plan tool: {e}", exc_info=True)
-        raise
-
-
-async def main():
-    await mcp.run_streamable_http_async()
-
+PORT = os.getenv("PORT", "3000")
 
 if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(PORT))
