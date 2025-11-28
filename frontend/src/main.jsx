fc41f9aafa7de65e3836e326e025bd4b53951bed
Geoff-Robin <jeffrobin132004@gmail.com>
2025-11-28 17:58:33 +0530

    pew

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
 import LandingPage from '@/pages/landingpage.jsx'
+import Login from './pages/login'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={ <LandingPage /> } />
+      <Route path="/login" element={ <Login />} />
     </Routes>
   </BrowserRouter>
 )
