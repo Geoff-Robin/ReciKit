3be997d9b210bde0da534ce0200a7d5c893ef855
Geoff-Robin <jeffrobin132004@gmail.com>
2025-11-29 11:50:23 +0530

    Added home

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import LandingPage from '@/pages/landingpage'
 import Login from '@/pages/login'
 import SignUp from '@/pages/signup.jsx'
 import './index.css'
+import MealPlanApp from './pages/MealPlan'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Route path="/" element={ <LandingPage /> } />
       <Route path="/login" element={ <Login />} />
       <Route path="/signup" element={ <SignUp />} />
+      <Route path="/home" element={<MealPlanApp />} />
     </Routes>
   </BrowserRouter>
 )
