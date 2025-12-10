a5003fc1dc1130a96a00c9c27745dfc80c7ab7c5
Geoff-Robin <jeffrobin132004@gmail.com>
2025-12-10 21:04:04 +0530

    changed address and port

--- recommendation_service/main.py
+++ recommendation_service/main.py
@@ -36,9 +36,9 @@ async def health_check():
 
 fast_api_app.mount("/mcp", mcp_app.streamable_http_app())
 fast_api_app.include_router(recommendation_route, prefix="/api")
-PORT = os.getenv("PORT", "3000")
+PORT = os.getenv("PORT", "8000")
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(fast_api_app, host="0.0.0.0", port=int(PORT))
+    uvicorn.run(fast_api_app, host="121.0.0.1", port=int(PORT))
